53d03e7e45fa38a1121e105c159e758cba0a4886
Den <denvanrain@gmail.com>
2021-08-18 15:21:00 +0300

    Add logic for Car

--- car.rb
+++ car.rb
@@ -1,2 +1,14 @@
-class Car
+require_relative 'transport'
+
+class Car < Transport
+  attr_accessor :number
+
+  def initialize
+    @speed = 50
+    @max_weight = 100
+  end
+
+  def available
+    Car.availability.cars
+  end
 end
