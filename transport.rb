e7f88e9f6cd30855364d7ff4f172ede749684128
Den <denvanrain@gmail.com>
2021-08-18 15:18:56 +0300

    Add Logic for Transport

--- transport.rb
+++ transport.rb
@@ -1,2 +1,18 @@
+require_relative 'vehicle_availabilities'
+
 class Transport
+  extend VehicleAvailabilities
+  attr_accessor :max_weight, :speed, :available
+
+  def delivery_time(distance)
+    (distance / speed.to_f).round(2)
+  end
+
+  def delivery_speed_per_kg
+    (speed.to_f / max_weight).round(2)
+  end
+
+  def available_vehicle
+    { cars: Transport.availability.cars, bikes: Transport.availability.bikes }
+  end
 end
