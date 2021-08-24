9c82ca531b43d26dbeacb1fd40ee77a6b29dbd8f
Den <denvanrain@gmail.com>
2021-08-24 09:51:38 +0300

    Update Transport
    Add initialize
    Add compare_delivery_speed_by_weight_with

--- lib/transport.rb
+++ lib/transport.rb
@@ -1,11 +1,21 @@
 class Transport
   attr_accessor :max_weight, :speed, :available
 
+  def initialize(transport)
+    @max_weight = transport.max_weight
+    @speed = transport.speed
+    @available = transport.available
+  end
+
   def delivery_time(distance)
-    (distance / speed.to_f).round(2)
+    (distance / @speed.to_f).round(2)
   end
 
   def delivery_speed_by_weight
-    (speed.to_f / max_weight).round(2)
+    (@speed.to_f / @max_weight).round(2)
+  end
+
+  def compare_delivery_speed_by_weight_with(transport)
+    self.delivery_speed_by_weight == transport.delivery_speed_by_weight
   end
 end
